c1868c6b6f152aefaeaa2f68f6c57ffc855421ff
Sami Al-Jamal <sami.m.aljamal@gmail.com>
2016-08-05 15:07:23 -0700

    fix style issue.

--- js/github-interface.js
+++ js/github-interface.js
@@ -2,11 +2,13 @@
 var gitHub = require('./../js/github.js').gitHubModule;
 
 $(document).ready(function(){
+  $("#results").hide();
   $("#form").submit(function(event){
     event.preventDefault();
     var username = $("#username").val();
     var result= new gitHub(username);
-    // result.getRepos();
+    result.getRepos();
     result.Getuserinfo();
+    $("#results").show();
   });
 });
